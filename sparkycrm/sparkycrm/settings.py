edc3ddafc111f9696b0966eaf122219a96823c15
Thomas <thomas.meche@gmail.com>
2019-10-12 12:43:22 -0600

    update setting.py

--- sparkycrm/sparkycrm/settings.py
+++ sparkycrm/sparkycrm/settings.py
@@ -121,8 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = 'C:/Users/Thomas/Documents/CRM/sparkycrm/staticfiles'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'), 
+]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
