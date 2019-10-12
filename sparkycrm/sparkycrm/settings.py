830918cff77ed9f5fea987020b0f4e713f488bfa
Thomas <thomas.meche@gmail.com>
2019-10-12 12:50:14 -0600

    update settings.py add django_heroku

--- sparkycrm/sparkycrm/settings.py
+++ sparkycrm/sparkycrm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,12 +124,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'), 
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'), 
+# ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'login_success'
+
+django_heroku.settings(locals())
